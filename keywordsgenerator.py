1be111f5e609b6561183f5e7063d1d83b78c9479
arwildo <arwildo@gmail.com>
2021-11-22 13:42:00 +0900

    create keywordsgenerator

---
+++ keywordsgenerator.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+exchanges = ["Binance", "Binance Futures", "KuCoin", "FTX", "Gate.io", "Tokocrypto", "Indodax"]
+words = ["referral code, ", "discount code, ", "trading fee discount, ", "cashback code, ", "bonus code", "referral link, "]
+
+def keywordsgen(exchanges, words):
+    for i in words:
+        for j in exchanges:
+            print(j + ' ' + i)
+
+keywordsgen(exchanges, words)
